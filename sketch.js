9d6b9a3adaf403fd9d3ee2fa2a2b7b7cdfd85f1d
Sidhant2007 <74358064+Sidhant2007@users.noreply.github.com>
2021-05-06 20:04:19 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,4 +1,8 @@
 var ground, Matter;
+var score=0;
+var particle;
+var gameState="Start";
+var count=0;
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
@@ -10,7 +14,7 @@ var divisions = [];
 var divisionHeight=300;
 
 function setup() {
-  createCanvas(480,800);
+  createCanvas(600,750);
   
   engine = Engine.create();
   world = engine.world;
@@ -30,7 +34,7 @@ function setup() {
         plinkos.push(new Plinko(j, 225));
         }
 
- ground = new Ground(240, 780, 480, 20);
+ ground = new Ground(300, 740, 600, 20);
 
 
 }
@@ -39,6 +43,46 @@ function draw() {
   background("black");  
   Engine.update(engine);
   ground.display();
+  fill("white")
+  textSize(20)
+  text("score:"+score,50,50)
+  text("500",30,550)
+  text("500",110,550)
+  text("200",190,550)
+  text("200",270,550)
+  text("200",350,550)
+  text("100",430,550)
+  text("100",510,550)
+  text("100",570,550)
+
+  if(particle!=null){
+    console.log("particle")
+    particle.display()
+    if(particle.body.position.y>740){
+      if(particle.body.position.x<150){
+        score=score+500
+        particle=null
+        if(count>=5){
+          gameState="end"
+        }
+      }
+      else if(particle.body.position.x>151&&particle.body.position.x<380){
+        score=score+200
+        particle=null
+        if(count>=5){
+          gameState="end"
+        }
+      }
+      else if(particle.body.position.x>381&&particle.body.position.x<600){
+        score=score+100
+        particle=null
+        if(count>=5){
+          gameState="end"
+        }
+      }
+    }
+  }
+
 
   for(var k = 0; k < divisions.length; k++){
   divisions[k].display()
@@ -47,13 +91,23 @@ function draw() {
   for(var j = 0; j < plinkos.length; j++){
     plinkos[j].display()
     }
-  if(frameCount % 100 === 0){
-  particles.push(new Particle(random(width/2-100, width/2+100),5, 10))
-  }
-  for(var r = 0; r < particles.length; r++){
-    particles[r].display()
-    }
+  
+  
+  
 
+  
 
   drawSprites();
+
+}
+
+function mousePressed()
+{
+  console.log(gameState)
+  if(gameState!=="end")
+  {
+    console.log(gameState)
+    count=count+1
+    particle = new Particle(mouseX,10,10,10);
+  }
 }
